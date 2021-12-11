4d2d832585d5b2d1ac7ac09d86524642fea598ee
Carmine <corvettomaligno@gmail.com>
2021-12-11 17:55:30 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+     
+     <div>
+       <h1 className='text-3xl text-center font-semibold mt-2'>Ciao da Carmine</h1>
+        <ul>
+          <li>Ciao</li>
+          <li>Come</li>
+          <li>Stai</li>
+        </ul>
+      </div>
+
+  )
+}
